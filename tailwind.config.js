78c9db4599eff5a9a8371a8cdb5e0d7134e02ec2
Sashikumar <yshashi30@gmail.com>
2024-07-07 17:29:30 +0800

    chore: Add Tailwind CSS with header and footer inside host

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './projects/host/src/**/*.{html,ts,scss}',
+    './projects/remote/src/**/*.{html,ts,scss}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
